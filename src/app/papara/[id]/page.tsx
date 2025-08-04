c2a3621c75ead8fe2a3ff98a2c0cf2c2762114d6
yavuzbirgol <yavuzbirgol@outlook.com>
2025-08-04 03:15:36 +0300

    first commit2 papara

--- src/app/papara/[id]/page.tsx
+++ src/app/papara/[id]/page.tsx
@@ -2,7 +2,7 @@ import PaymentPagePapara from "../../components/paymentPagePapara";
 
 export default async function Home({ params }: { params: Promise<{ id: string }> }) {
   const resolvedParams = await params;
-  const data = await fetch('http://localhost:8003/deposit/information/' + resolvedParams.id);
+  const data = await fetch('https://api.vitahavale.io/deposit/information/' + resolvedParams.id);
   const dataJson = await data.json();
   if (dataJson.statusCode === 400) {
     return <div>Hata: {dataJson.message}</div>;
